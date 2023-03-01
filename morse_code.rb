fa9835baa6dadf46d5fdf0a7e0bbc4ef5d1a66bf
admirerbrown <admirerbrown@gmail.com>
2023-03-01 17:14:49 +0000

    create decode_word method && decode_msg method

--- morse_code.rb
+++ morse_code.rb
@@ -30,3 +30,25 @@ def decode_char(input)
 
   morse_code[input]
 end
+
+def decode_word(word)
+  final_word = ''
+  word.split(/ /).each do |n|
+  final_word += decode_char(n)
+  end
+  final_word
+end
+
+def decode_msg(msg)
+  message = ""
+  msg = msg.split(/   /).each do |n|
+  message += decode_word(n)
+  message += " "
+  end
+  message
+end
+
+decode_msg("-- -.--   -. .- -- .")
+# => MY NAME
+decode_msg(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+# => A BOX FULL OF RUBIES
