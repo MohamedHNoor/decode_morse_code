128a804d3d6afd6930f55ead420af876cb5109c6
admirerbrown <admirerbrown@gmail.com>
2023-03-01 17:38:01 +0000

    fix linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -1,31 +1,10 @@
-def decode_char(input) 
+def decode_char(input)
   morse_code = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
 
   morse_code[input]
@@ -34,24 +13,25 @@ end
 def decode_word(word)
   final_word = ''
   word.split(/ /).each do |n|
-  final_word += decode_char(n)
+    final_word += decode_char(n)
   end
   final_word
 end
 
 def decode_msg(msg)
-  message = ""
-  msg = msg.split(/   /).each do |n|
-  message += decode_word(n)
-  message += " "
+  message = ''
+  msg = msg.split(/   /)
+  msg.each do |n|
+    message += decode_word(n)
+    message += ' '
   end
   message
 end
 
-decode_msg("-- -.--   -. .- -- .")
+decode_msg('-- -.--   -. .- -- .')
 
 # => MY NAME
 
-decode_msg(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+decode_msg('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 
 # => A BOX FULL OF RUBIES
